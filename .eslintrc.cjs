3f4191a03fe4f3e8e35f52ba59d95724eaf80daf
Lucas zapico <12178702+LucasZapico@users.noreply.github.com>
2024-12-02 00:42:13 +0000

    feat(lint): enhance linter
    - add rule preferences

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,55 +6,70 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   env: {
     browser: true,
     commonjs: true,
     es6: true,
   },
-  ignorePatterns: ["!**/.server", "!**/.client"],
-
   // Base config
-  extends: ["eslint:recommended"],
-
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:prettier/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  ignorePatterns: [
+    ".eslintrc.json",
+    "node_modules/",
+    "build/",
+    ".next/",
+    "out/",
+    "public/",
+    "next.config.js",
+    "next-env.d.ts",
+    "**/**.*.jsx",
+    "**/**.*.js",
+  ],
   overrides: [
     // React
     {
-      files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
       extends: [
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
       ],
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      plugins: ["react", "jsx-a11y", "prettier", "@typescript-eslint"],
       settings: {
-        react: {
-          version: "detect",
-        },
         formComponents: ["Form"],
-        linkComponents: [
-          { name: "Link", linkAttribute: "to" },
-          { name: "NavLink", linkAttribute: "to" },
-        ],
         "import/resolver": {
           typescript: {},
         },
+        linkComponents: [
+          {
+            linkAttribute: "to",
+            name: "Link",
+          },
+          {
+            linkAttribute: "to",
+            name: "NavLink",
+          },
+        ],
+        react: {
+          version: "detect",
+        },
       },
-    },
-
-    // Typescript
+    }, // Typescript
     {
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+      ],
       files: ["**/*.{ts,tsx}"],
-      plugins: ["@typescript-eslint", "import"],
       parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint", "import"],
       settings: {
         "import/internal-regex": "^~/",
         "import/resolver": {
@@ -66,19 +81,37 @@ module.exports = {
           },
         },
       },
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-      ],
-    },
-
-    // Node
+    }, // Node
     {
-      files: [".eslintrc.cjs"],
       env: {
         node: true,
       },
+      files: [".eslintrc.js"],
     },
   ],
+  parserOptions: {
+    project: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  root: true,
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript/no-unsafe-member-access": "off",
+    "import/prefer-default-export": "off",
+    "no-unused-vars": "off",
+    "prettier/prettier": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/no-unescaped-entities": "off",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+  },
 };

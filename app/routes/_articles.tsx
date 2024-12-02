1f2aa5ee23f6c3e0f3372254359dac855ac9fe22
LucasZapico <hello@lucaszapico.space>
2024-12-02 10:37:51 -0800

    feat(layout): add article wrapper

--- app/routes/_articles.tsx
+++ app/routes/_articles.tsx
@@ -1,12 +1,13 @@
-
 import { Outlet } from "@remix-run/react"
 import { MDXProvider } from "@mdx-js/react"
+import { MdxComponents } from "@/components/mdx/MDXReducer"
 
 export default function Articles() {
-
   return (
-      <MDXProvider>
-          <Outlet />
-      </MDXProvider>
+    <MDXProvider components={MdxComponents}>
+      <div className="py-10 px-10 prose lg:prose-xl">
+        <Outlet />
+      </div>
+    </MDXProvider>
   )
 }
